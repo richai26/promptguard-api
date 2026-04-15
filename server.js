c89e2f55c96226ff695f314a145b76b4933eb7c2
Your Name <richarddavidhoey@gmail.com>
2026-04-15 21:04:41 +0100

    Add manual CORS headers

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(helmet({ crossOriginResourcePolicy: false }));
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});app.use(express.json());
 
 app.use(rateLimit({
   windowMs: 60 * 1000,
